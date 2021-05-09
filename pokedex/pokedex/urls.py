7e535ec4aff02990db7b0968b0019ecbd255d7cc
ToniIvars <toniivaju@gmail.com>
2021-05-09 13:58:48 +0200

    Added an index view

--- pokedex/pokedex/urls.py
+++ pokedex/pokedex/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pokedex/', include('basics.urls')),
-    path('moves/', include('moves.urls'))
+    path('moves/', include('moves.urls')),
+    path('', views.index, name='pokedex_index')
 ]
