65ea30f5b4f43356b0288f4251372f69aacbfb5b
ToniIvars <toniivaju@gmail.com>
2021-05-11 16:17:30 +0200

    Replaced the learning method titles by buttons that show or hide their table

--- pokedex/static/basics/moves.js
+++ pokedex/static/basics/moves.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const gen_buttons = Array.from(document.querySelectorAll('.gen-buttons'))
     const gen_tables = Array.from(document.querySelectorAll('.gen-tables'))
 
+    const dropdown_buttons = Array.from(document.querySelectorAll('.dropdown-toggle'))
+
     gen_buttons.slice(-1).pop().classList.add('button-disabled')
     gen_buttons.slice(-1).pop().disabled = true
     document.getElementById(gen_buttons.slice(-1).pop().id.slice(4)).classList.remove('d-none')
@@ -21,6 +23,19 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById(btn.id.slice(4)).classList.remove('d-none')
 
             gen_title.innerHTML = `Generation ${btn.id.slice(-1)}`
+        }
+    })
+
+    dropdown_buttons.forEach(btn => {
+        btn.onclick = () => {
+            const table_id = btn.id.split('-').slice(1).join('-')
+            const table = document.getElementById(table_id)
+
+            if (table.classList.contains('d-none')) {
+                table.classList.remove('d-none')
+            } else {
+                table.classList.add('d-none')
+            }
         }
     })
 })
