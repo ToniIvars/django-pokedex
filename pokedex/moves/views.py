9b0d70bf908100447d254291d8249f268bf53316
ToniIvars <toniivaju@gmail.com>
2021-05-11 22:49:04 +0200

    Added the functionality of the moves app

--- pokedex/moves/views.py
+++ pokedex/moves/views.py
@@ -5,5 +5,23 @@ import json, requests
 
 # Create your views here.
 def move(request, name):
-    data = {'name':name}
+    res = requests.get(f'https://pokeapi.co/api/v2/move/{name}/')   
+
+    if res.status_code != 200:
+        messages.error(request, 'The move you are trying to find does not exist')
+        return redirect('pokedex_index')
+
+    data = res.json()
+
+    move_data = {
+        'accuracy': data['accuracy'],
+        'power': data['power'],
+        'pp': data['pp'],
+        'type': data['type']['name'],
+        'category': data['damage_class']['name'],
+        'description': [entry['flavor_text'].replace('\n', ' ') for entry in data['flavor_text_entries'] if entry['language']['name'] == 'en' and  entry['version_group']['name'] =='ultra-sun-ultra-moon'][0],
+        'effect': data['effect_entries'][0]['effect']
+    }
+
+    data = {'name':name, 'move_data':move_data}
     return render(request, 'moves/move.html', {'data':data})
